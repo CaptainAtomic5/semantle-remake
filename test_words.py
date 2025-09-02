4c568102c499312c55e1a549425ef58bbc3d4d40
Lalit J <lalitjulapalli5@gmail.com>
2025-09-01 22:33:24 -0400

    test_words.py update

--- test_words.py
+++ test_words.py
@@ -20,3 +20,12 @@ word1 = word1.vector
 word2 = word2.vector
 
 print(f"Distance between the words: {distance(word1, word2): .3f}")
+
+# the functionality I want to bring next:
+# return what part of speech the word is
+def get_part_of_speech(word):
+    doc = nlp(word)
+    return doc.pos   
+# select a random word
+# compare the word to whatever word the user specifies
+# create the notion of "warmer/colder"
